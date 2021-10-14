eee3d97e9b1eca58f37030ed73bd11a768b16db0
Jason Edson <jaysonedson@gmail.com>
2021-10-14 12:16:29 -0700

    Show user's Google avatar on the homepage
    Change-Id: I16f3e241d2d2a4a25977fe7428687b88c8fde0c6
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- src/com/android/settings/accounts/AccountFeatureProviderImpl.java
+++ src/com/android/settings/accounts/AccountFeatureProviderImpl.java
@@ -10,11 +10,11 @@ import com.android.settings.overlay.FeatureFactory;
 public class AccountFeatureProviderImpl implements AccountFeatureProvider {
     @Override
     public String getAccountType() {
-        return FeatureFactory.getAppContext().getString(R.string.account_type);
+        return "com.google";
     }
 
     @Override
     public Account[] getAccounts(Context context) {
-        return AccountManager.get(context).getAccountsByType(getAccountType());
+        return AccountManager.get(context).getAccountsByType("com.google");
     }
 }
